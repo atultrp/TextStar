1036d8226ebc3c33e31b2f75b5b56b7520e63c04
Atul Tripathi <atul31815@gmail.com>
2021-10-28 20:36:51 +0530

    Version 1.4

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -21,10 +21,7 @@ export default function TextForm(props) {
     }
 
     const handleCopy = () => {
-        var text = document.getElementById("myBox");
-        text.select();
-        document.getSelection().removeAllRanges();
-        navigator.clipboard.writeText(text.value);
+        navigator.clipboard.writeText(text);
         props.showAlert("Text copied!","success");
     }
 
